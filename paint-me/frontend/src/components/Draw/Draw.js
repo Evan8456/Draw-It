79aa961df42b2a0ca7a213c124da5702098c3657
erictan1 <erictanorange1999@gmail.com>
2022-03-19 17:22:23 -0400

    sync drawing with rooms

--- paint-me/frontend/src/components/Draw/Draw.js
+++ paint-me/frontend/src/components/Draw/Draw.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import "./Draw.css";
 import { useNavigate } from 'react-router-dom';
 import api from "../../api"
+import {io} from 'socket.io-client';
 
 export function Draw() {
   const canvasRef = useRef();
@@ -11,18 +12,32 @@ export function Draw() {
   const [thickness, setThick] = useState(null);
   const [startX, setStartX] = useState(null);
   const [startY, setStartY] = useState(null);
+  const [s, setS] = useState(null);
+
+  const [room, setR] = useState(null);
 
   let navigate = useNavigate();
 
-  useEffect(() => {
-      api.authenticate((res) => {
-          console.log("Logged in");
-      }, (err) => {
-          navigate("/")
-      })
-  });
 
   useEffect(() => {
+
+    api.authenticate((res) => {
+
+      const socket = io(process.env.REACT_APP_SOCKET)
+      setS(socket);
+
+      socket.on('drawing', goDraw);
+      socket.on("connect", () =>{
+        console.log(`connected with id :${socket.id}`);
+        socket.emit('join-room',"testRoom");
+        console.log(`socket join room: testRoom`);
+        setR("testRoom");
+      });
+      
+    }, (err) => {
+        navigate("/")
+    })
+
     const canvas = canvasRef.current;
     canvas.width = 600 * 2;
     canvas.height = 600 * 2;
@@ -36,6 +51,40 @@ export function Draw() {
 
 
   }, []);
+
+  function goDraw(data){
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.strokeStyle= data.color;
+    context.lineWidth = data.thickness;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(data.x0, data.y0);
+    contextRef.current.lineTo(data.x1, data.y1);
+    contextRef.current.stroke();
+    contextRef.current.closePath();
+    context.strokeStyle= colorRef.current.value;
+    context.lineWidth = thickness;
+
+  }
+  function joinRoom1(){
+    s.emit('join-room',"testRoom1");
+    setR("testRoom1");
+    clearCanvas();
+    console.log(`socket join room: testRoom 1`);
+  }
+
+  function joinRoom2(){
+    s.emit('join-room',"testRoom2");
+    setR("testRoom2");
+    clearCanvas();
+    console.log(`socket join room: testRoom 2`);
+  }
+
+  function clearCanvas(){
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
   const changeColor = (color) => {
     const canvas = canvasRef.current;
@@ -82,11 +131,11 @@ export function Draw() {
     setIsDrawing(false);
   };
 
-  const draw = (e ) => {
+  const draw = (e) => {
     if (!isDrawing) {
       return;
     }
-    console.log(e);
+    console.log("currently drawing on room: " + room);
     contextRef.current.beginPath();
     contextRef.current.moveTo(startX, startY);
     const { offsetX, offsetY } = e.nativeEvent;
@@ -94,6 +143,19 @@ export function Draw() {
     setStartY(offsetY);
     contextRef.current.lineTo(offsetX, offsetY);
     contextRef.current.stroke();
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    s.emit('drawing', {
+      x0: startX,
+      y0: startY,
+      x1: offsetX,
+      y1: offsetY,
+      color: context.strokeStyle,
+      thickness: context.lineWidth
+
+    }, room);
     
   };
 
@@ -127,6 +189,9 @@ export function Draw() {
         />
 
         <button onClick={() => changeColor("#FFFFFF")}>Erase</button>
+
+        <button onClick={() => joinRoom1()}>Test Room 1</button>
+        <button onClick={() => joinRoom2()}>Test Room 2</button>
       </div>
     </>
   );
