3f83a2a03818d83e1e8e81c62f96874f95343f8d
erictan1 <erictanorange1999@gmail.com>
2022-03-28 15:47:11 -0400

    separate server and dev code in backend

--- paint-me/backend/app.js
+++ paint-me/backend/app.js
@@ -29,51 +29,40 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const cookie = require('cookie');
-var session = require("express-session")({
-  secret: process.env.SESSION_SECRET_KEY,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    samesite : 'strict'
-   }
-});
-
 var sharedsession = require("express-socket.io-session");
 
 //DEV ONLY
 if(process.env.ENVIRONMENT == "dev") {
+  const httpServer = require("http").createServer(app);
+  var session = require("express-session")({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      samesite : 'strict'
+     }
+  });
   // cors for dev
   const cors = require('cors')
   app.use(cors({
     origin:['http://localhost:3000', 'https://studio.apollographql.com'],
     credentials: true
   }));
-
-  const io = require("socket.io")(3002, {
+  const io = require("socket.io")(httpServer, {
+    path: '/',
     cors:{
       origin: ['http://localhost:3000'],
     },
     cookie: true
-  })
-
-
+  });
+  console.log("Dev")
+  // both use
+  //socket io
   io.use(sharedsession(session, {
     autoSave:true
   })); 
-
-  app.use(session);
-
-  async function start() {
-    const server = new ApolloServer({ typeDefs, resolvers,
-    context: ({req, res}) => ({req, res})});
-    await server.start()
-    
-    server.applyMiddleware({ app });
-  }
-  start();
-  
   io.on("connection", socket =>{
     console.log(socket.id)
     socket.on("drawing", (data, room) =>{
@@ -93,40 +82,97 @@ if(process.env.ENVIRONMENT == "dev") {
       console.log(socket.rooms);
     })
   })
-  
-
-
+  // apollo server
+  app.use(session);
+  async function start() {
+    const server = new ApolloServer({ typeDefs, resolvers,
+    context: ({req, res}) => ({req, res})});
+    await server.start()
+    
+    server.applyMiddleware({ app });
+  }
+  start();
+  httpServer.listen(3002, () => {
+    console.log("Websocket started at port ", 3002)
+  });
 } else {
-  console.log("????")
-  app.use(session({
-    secret: 'This is the final project',
-    resave: false,
+  var privateKey  = fs.readFileSync('/etc/letsencrypt/live/draw-it.me/privkey.pem');
+  var certificate = fs.readFileSync('/etc/letsencrypt/live/draw-it.me/cert.pem');
+  var chain = fs.readFileSync('/etc/letsencrypt/live/draw-it.me/chain.pem');
+  var httpsServer = 
+  require('https').createServer({key: privateKey,
+    cert: certificate,
+    ca: chain,
+    requestCert: false,
+    rejectUnauthorized: false },app);
+  var session = require("express-session")({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: true,
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
       secure: true,
       samesite : 'strict'
      }
-   }));
+  });
+  const io = require("socket.io")(httpsServer, {
+    path: '/',
+    cookie: true
+  });
+  console.log("Server")
+  // both use
+  //socket io
+  io.use(sharedsession(session, {
+    autoSave:true
+  })); 
+  io.on("connection", socket =>{
+    console.log(socket.id)
+    socket.on("drawing", (data, room) =>{
+      console.log("currently drawing on room: " + room);
+      socket.to(room).emit('drawing', data);
+      //io.broadcast.emit('test2',param1);
+      //socket.to(room);
+    })
+    socket.on("join-room", (room) =>{
+      var rooms = socket.rooms;
+      console.log(socket.rooms);
+      rooms.forEach(element => {
+        console.log(element);
+        socket.leave(element);
+      });
+      socket.join(room);
+      console.log(socket.rooms);
+    })
+  })
+  // apollo server
+  app.use(session);
+  async function start() {
+    const server = new ApolloServer({ typeDefs, resolvers,
+    context: ({req, res}) => ({req, res})});
+    await server.start()
+    
+    server.applyMiddleware({ app });
+  }
+  start();
+
+  httpsServer.listen(3002, () => {
+    console.log("Websocket started at port ", 3002)
+  });
 }
- 
+
+// rest api
 var isAuthenticated = function(req, res, next) {
   if (!req.session.username) return res.status(401).end("access denied");
   next();
 };
-
- app.use(function (req, res, next){
-   var cookies = cookie.parse(req.headers.cookie || '');
-   console.log(req.session.username);
-   req.username =(req.session.user)? req.session.user.username : ''
-   console.log("HTTP request", req.username, req.method, req.url, req.body);
-   next();
- });
-
- 
-
-
-  app.get('/signout/',  isAuthenticated,function (req, res, next) {
+app.use(function (req, res, next){
+  var cookies = cookie.parse(req.headers.cookie || '');
+  console.log(req.session.username);
+  req.username =(req.session.user)? req.session.user.username : ''
+  console.log("HTTP request", req.username, req.method, req.url, req.body);
+  next();
+});
+app.get('/signout/',  isAuthenticated,function (req, res, next) {
     req.session.destroy();
     res.setHeader('Set-Cookie', cookie.serialize('username', '', {
           path : '/', 
@@ -136,9 +182,6 @@ var isAuthenticated = function(req, res, next) {
     return res.json({});
     
 });
-
-
-
 app.post('/signup/',  function (req, res, next) {
   console.log("test");
   var username = req.body.username;
@@ -165,7 +208,6 @@ app.post('/signup/',  function (req, res, next) {
 
     });
 });
-
 app.post('/signin/',  function (req, res, next) {
    
   var username = req.body.username;
@@ -191,7 +233,6 @@ app.post('/signin/',  function (req, res, next) {
         
     });
 });
-
 app.get('/authenticate/', function(req, res, next){
   console.log("authenticate session")
   console.log(req.session)
@@ -201,8 +242,6 @@ app.get('/authenticate/', function(req, res, next){
     return res.status(401).send({status:"failure"});
   }
 });
-
-
 
 
 
