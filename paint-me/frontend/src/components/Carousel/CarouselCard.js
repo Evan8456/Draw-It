b854534868e2cf1fe4623706590123aaa26541d6
unknown <evanng8456@gmail.com>
2022-04-03 17:26:06 -0400

    fixed overflow bug

--- paint-me/frontend/src/components/Carousel/CarouselCard.js
+++ paint-me/frontend/src/components/Carousel/CarouselCard.js
@@ -8,11 +8,15 @@ export default function CarouselCard(props) {
     let Twitter = chakra(FaTwitter);
     let Download = chakra(FaDownload);
     const navigate = useNavigate();
-
+    let display_title = props.title.substring(0,20);
+    if(props.title.length >20){
+        display_title = props.title.substring(0,20) +"...";
+    }
+   
 
     return (
         <Flex as={'button'} width={props.width} justifyContent="center" alignItems="center" flexDirection="column" boxShadow="lg" borderRadius="2xl" onClick={() => navigate(props.drawpath, {state: {id: props._id, load: true}})}>
-            <Heading size="md">{props.title}</Heading>
+            <Heading size="md">{display_title}</Heading>
             <Image src={props.image} alt="Drawing" height="50%" marginTop="2" marginBottom="2"/>
             
         </Flex>
